b2b43509d41318a6b155d61cc55f468c36f06cd3
Jialin Yang <yangjialin94@gmail.com>
2021-02-09 13:33:34 -0600

    update

--- codedaddies_list/settings.py
+++ codedaddies_list/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path, os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like› this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
